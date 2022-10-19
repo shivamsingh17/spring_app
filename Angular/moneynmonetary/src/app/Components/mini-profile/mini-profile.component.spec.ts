efe7a66fcfaa600e6698aa3efd0e0c5e641b7806
Arghya Bandyopadhyay <arghyab13bga@gmail.com>
2022-10-19 07:49:10 +0530

    Create mini-profile.component.spec.ts

---
+++ Angular/moneynmonetary/src/app/Components/mini-profile/mini-profile.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MiniProfileComponent } from './mini-profile.component';
+
+describe('MiniProfileComponent', () => {
+  let component: MiniProfileComponent;
+  let fixture: ComponentFixture<MiniProfileComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MiniProfileComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MiniProfileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
