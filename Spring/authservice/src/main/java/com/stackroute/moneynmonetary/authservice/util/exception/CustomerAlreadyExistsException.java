2734f4a181a7b28cf3762aa4c726a17f28b1c179
Arghya Bandyopadhyay <arghyab13bga@gmail.com>
2022-11-17 20:16:20 +0530

    Create CustomerAlreadyExistsException.java

---
+++ Spring/authservice/src/main/java/com/stackroute/moneynmonetary/authservice/util/exception/CustomerAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.stackroute.moneynmonetary.authservice.util.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+@ResponseStatus(code = HttpStatus.CONFLICT,reason = "Customer already exists")
+public class CustomerAlreadyExistsException extends Exception{
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+}
