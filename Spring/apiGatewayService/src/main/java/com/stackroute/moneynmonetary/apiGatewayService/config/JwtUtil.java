fc52419bb930ccef9de2679a9483eba5519247f8
Arghya Bandyopadhyay <arghyab13bga@gmail.com>
2022-11-17 20:13:59 +0530

    Create JwtUtil.java

---
+++ Spring/apiGatewayService/src/main/java/com/stackroute/moneynmonetary/apiGatewayService/config/JwtUtil.java
@@ -0,0 +1,51 @@
+package com.stackroute.moneynmonetary.apiGatewayService.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+
+//    @Value("${jwt.secret}")
+    private final String secret="moneynmonetary";
+
+    public String extractEmailId(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+    public Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return this.getAllClaimsFromToken(token).getExpiration().before(new Date());
+    }
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String emailId = extractEmailId(token);
+        return (emailId.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public boolean isInvalid(String token) {
+        return this.isTokenExpired(token);
+    }
+
+}
