b6e49d5fc1009475f723588ce16a81d58fa0f36e
Arghya Bandyopadhyay <arghyab13bga@gmail.com>
2022-11-17 20:00:53 +0530

    Create customer.ts

---
+++ Angular/moneynmonetary/src/app/Models/customer.ts
@@ -0,0 +1,21 @@
+export interface Customer {
+    id:String;
+    password:String;
+    emailId:String;
+    mobileNo:String;
+    firstName:String;
+    lastName:String;
+    address:String;
+    city:String;
+    state:String;
+    country:String;
+    zipcode:String;
+    dob:Date;
+    accNo:number;
+    ifscCode:String;
+    profileUrl:String;
+    jwtToken: string;
+    checkinBalance:number;
+    savingsBalance:number;
+    currency:String;
+}
