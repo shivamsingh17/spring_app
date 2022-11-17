93b3474622ac15700ed6b22f52be57b377424e43
Arghya Bandyopadhyay <arghyab13bga@gmail.com>
2022-11-17 20:16:24 +0530

    Create CustomerNotExistsException.java

---
+++ Spring/authservice/src/main/java/com/stackroute/moneynmonetary/authservice/util/exception/CustomerNotExistsException.java
@@ -0,0 +1,12 @@
+package com.stackroute.moneynmonetary.authservice.util.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND,reason = "Customer with specified details not found")
+public class CustomerNotExistsException extends Exception{
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+}
