8584e5865b160bc6944e0cd845f668ae14b01cab
Arghya Bandyopadhyay <arghyab13bga@gmail.com>
2022-11-17 20:14:02 +0530

    Create RouterValidator.java

---
+++ Spring/apiGatewayService/src/main/java/com/stackroute/moneynmonetary/apiGatewayService/config/RouterValidator.java
@@ -0,0 +1,22 @@
+package com.stackroute.moneynmonetary.apiGatewayService.config;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouterValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/api/v1/authentication/authenticate",
+            "/api/v1/authentication/register"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
+}
