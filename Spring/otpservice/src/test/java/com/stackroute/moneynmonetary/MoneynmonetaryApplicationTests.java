098d788de98b8927d22544aa9c17fd58d1b5f17f
Arghya Bandyopadhyay <arghyab13bga@gmail.com>
2022-11-23 23:14:50 +0530

    Create MoneynmonetaryApplicationTests.java

---
+++ Spring/otpservice/src/test/java/com/stackroute/moneynmonetary/MoneynmonetaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.moneynmonetary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneynmonetaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
