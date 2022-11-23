773b25013a85d0bb1a4ef438348a3de8e83a7c84
Arghya Bandyopadhyay <arghyab13bga@gmail.com>
2022-11-23 23:11:25 +0530

    Create GlobalKeyValuesRepository.java

---
+++ Spring/customerservice/src/main/java/com/stackroute/moneynmonetary/customerservice/repository/GlobalKeyValuesRepository.java
@@ -0,0 +1,10 @@
+package com.stackroute.moneynmonetary.customerservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface GlobalKeyValuesRepository extends JpaRepository<GlobalKeyValues, UUID>{
+    Optional<GlobalKeyValues> findByKeyString(String keyString);
+}
